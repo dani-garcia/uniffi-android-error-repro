75fd33d5c2200f602bf4b0af3a11ad58e5b82cd5
Daniel García <dani-garcia@users.noreply.github.com>
2025-09-03 17:11:12 +0200

    Maybe fix?

--- crates/crate-a/src/lib.rs
+++ crates/crate-a/src/lib.rs
@@ -10,3 +10,10 @@ pub enum TestError {
 fn add(a: u32, b: u32) -> u32 {
     a + b
 }
+
+/* Uncomment this and it fixes the problem, the emitted code now does use Exception
+#[uniffi::export]
+fn test() -> Result<(), crate_b::CrateBError> {
+    Err(crate_b::CrateBError::CrateBScrewedUp(1))
+}
+*/
--- crates/crate-b/src/lib.rs
+++ crates/crate-b/src/lib.rs
@@ -1,6 +1,16 @@
+use std::fmt::Display;
+
 uniffi::setup_scaffolding!();
 
-#[derive(uniffi::Error)]
+#[derive(uniffi::Error, Debug)]
 pub enum CrateBError {
     CrateBScrewedUp(u64),
+}
+
+impl Display for CrateBError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CrateBError::CrateBScrewedUp(code) => write!(f, "CrateB screwed up with code {}", code),
+        }
+    }
 }
